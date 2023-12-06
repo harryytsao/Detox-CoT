ae7a9de45bd2b8cf6ec36a0e9558e4bc95643052
harry <harrytsao12345@gmail.com>
2023-12-05 19:47:14 -0500

    update perspective_api.sh

--- utils/perspective_api.sh
+++ utils/perspective_api.sh
@@ -1,13 +1,13 @@
 python perspective_api_span.py \
 --file ../dataset/span_cnn_train.json \
 --output ../dataset/span_cnn_train_score.json \
---api_key AIzaSyAAaNzAf2TKbAJb0SqgQEmtj608fLY4Fe4 \
+--api_key your-api-key \
 --api_rate 90000 \
 --process 1
 
 python perspective_api_span.py \
 --file ../dataset/span_cnn_test.json \
 --output ../dataset/span_cnn_test_score.json \
---api_key AIzaSyAAaNzAf2TKbAJb0SqgQEmtj608fLY4Fe4 \
+--api_key your-api-key \
 --api_rate 9000 \
 --process 1
